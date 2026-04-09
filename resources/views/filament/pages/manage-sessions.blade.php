b13f9ed61b4b6c0e2b2f1d57e355c3f482c72e9e
Bhre Astrajingga <bhre.astrajingga@gmail.com>
2026-04-09 14:50:38 +0700

    ganti

--- resources/views/filament/pages/manage-sessions.blade.php
+++
@@ -1,4 +0,0 @@
-<x-filament::page>
-    {{ $this->table }}
-    <p class="text-xs text-gray-500 mt-4">Gunakan tombol "Logout Other Devices" di header untuk keluar dari perangkat lain.</p>
-</x-filament::page>
